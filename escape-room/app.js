2a339e76fc97a1dce155611ac9ed330e6cb70671
Juan Aviles <avilesjuan628@gmail.com>
2022-05-22 17:47:58 -0400

    saves final changes

--- escape-room/app.js
+++ escape-room/app.js
@@ -10,14 +10,14 @@ const dead = "you are dead";
 const escape = "Congrats, you have escaped!";
 // create a loop with if statements involved. 
 while (!outcome) {
-    let choiceIndex = readline.keyInSelect(choices, "What do you do? ")
-    if (choiceIndex === 0) {
+    let choicesIndex = readline.keyInSelect(choices, "What do you do? ")
+    if (choicesIndex === 0) {
         console.log(`Something ate your hand.`)
         outcome = dead;      
-    } else if (choiceIndex === 1) {
+    } else if (choicesIndex === 1) {
         console.log("you found the key under the mattress!")
         outcome = escape;
-    } else if (choiceIndex === 2) {
+    } else if (choicesIndex === 2) {
         console.log("you tried to open the door and nothing happened.")
     } else {
         console.log("you're stuck in the room for years\n")
